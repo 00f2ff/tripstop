46f27c4cc71a9a23d178c4fe80d27dcf12abcdd9
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-10-02 22:42:25 -0400

    Got google directions api working for basic case

--- public/tripstop/routes/yelpRoute.js
+++ public/tripstop/routes/yelpRoute.js
@@ -4,7 +4,7 @@ exports.findRestaurants = function(request, response) {
 	var set_parameters = {
 		location: request.params.location, // will change to ll later
 		term: 'restaurants', // default for now, but could be changed in future to param
-		radius_filter: request.params.radius_filter
+		radius_filter: request.params.radius_filter // needs to be converted to meters
 	}
 	yelp.request_yelp(set_parameters, function(err, res, body) { 
 		response.end(body);
