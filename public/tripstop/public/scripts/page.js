1c06109e623a3424f064f1a4d61ea02aa2208d3d
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-10-06 22:16:45 -0400

    created way to find stop point that isn't nearly robust enough (aka it doesn't take into account road speed to find the exact stopping point along a large stretch of highway), but should suffice for the MVP demo; UI won't show up correctly yet, but data is presented in console

--- public/tripstop/public/scripts/page.js
+++ public/tripstop/public/scripts/page.js
@@ -1,29 +1,68 @@
 /* in future i'd want to let people choose between tolls, ferries, etc, choose alternate routes, etc */
   $(function() {
     var directionsService = new google.maps.DirectionsService();
-     var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
 
-     var map = new google.maps.Map(document.getElementById('map'), {
-       zoom:7,
-       mapTypeId: google.maps.MapTypeId.ROADMAP
-     });
+    var map = new google.maps.Map(document.getElementById('map'), {
+     zoom:7,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-     directionsDisplay.setMap(map);
-     directionsDisplay.setPanel(document.getElementById('panel'));
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('panel'));
 
-     function makeRequest(origin, destination) {
-        var request = {
-         origin: origin, 
-         destination: destination, // add waypoint soon
-         travelMode: google.maps.DirectionsTravelMode.DRIVING
-       };
+    function findStopStep(steps) {
+      var time = parseInt($('input[name="hours"]').val(),10) * 60 * 60; // conversion to step duration value (seconds)
+      var index = 0;
+      while (time > 0) {
+        time -= steps[index].duration.value;
+        index++;
+      }
+      index -= 1; // go back to step that took time below 0 (so farther along than we want)
+      console.log(steps[index]);
+      // find yelp data
+      var radius_filter = parseInt($('input[name="radius"]').val(),10) * 1609.34; // convert to meters
+      var ll = steps[index].end_location.lat() + ',' + steps[index].end_location.lng();
+      $.ajax({
+        url: '/stop/'+ll+'/'+radius_filter,
+        type: "GET",
+        success: function(result) {
+          stuff = JSON.parse(result);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(textStatus, errorThrown);
+        }
+      });
+    }
 
-       directionsService.route(request, function(response, status) {
-         if (status == google.maps.DirectionsStatus.OK) {
-           directionsDisplay.setDirections(response);
-         }
-       });
-     }
+    function makeRequest(origin, destination, finalRoute) {
+      var request = {
+        origin: origin, 
+        destination: destination, // add waypoint soon
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+      };
+
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          var steps = response.routes[0].legs[0].steps; // this would differ if there were more routes/legs
+          var stopStep = findStopStep(steps);
+          if (finalRoute) { // not initial find
+            directionsDisplay.setDirections(response); 
+          }
+        }
+      });
+    }
+
+    $('button#find-restaurants').click(function(ev) {
+      var hours = $('input[name="hours"]').val(),
+          radius = $('input[name="radius"]').val(),
+          origin = $('input[name="origin"]').val(), 
+          destination = $('input[name="destination"]').val();
+      // generate map
+      // makeRequest(origin, destination);
+      makeRequest('washington dc','pittsburgh pa', false);
+      ev.preventDefault();
+    })
 
     $('input[name="rest-button"]').click(function(ev) {
       ev.preventDefault();
--- public/tripstop/routes/yelpRoute.js
+++ public/tripstop/routes/yelpRoute.js
@@ -2,7 +2,7 @@ var yelp = require('../models/yelp.js');
 
 exports.findRestaurants = function(request, response) {
 	var set_parameters = {
-		location: request.params.location, // will change to ll later
+		ll: request.params.ll,
 		term: 'restaurants', // default for now, but could be changed in future to param
 		radius_filter: request.params.radius_filter // needs to be converted to meters
 	}
