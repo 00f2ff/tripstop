08e63c2f63dc7cba349cf79157ebf99c7bb55bdc
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-10-06 23:49:50 -0400

    needed to manually add

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 var express = require('express'),
-  morgan = require('morgan'),
-  path = require('path');
+    morgan  = require('morgan'),
+    path = require('path');
 
-// This app uses the expressjs framework
-app = express();
-// Log requests
-app.use(morgan('[:date] :method :url :status'));
-// Process static files which will be found in the public subdirectory
-app.use(express.static(path.join(__dirname, 'public')));
+// Create a class that will be our main application
+var SimpleStaticServer = function() {
 
-/*
- * This simple server will work on your laptop if you have installed
- * Nodejs, or deployed to OpenShift.  OpenShift will provide environment
- * variables indicating the IP address and PORT to use.  If those variables
- * are not available (e.g. when you are testing the application on your
- * laptop) then use default values of localhost (127.0.0.1) and
- * port 33333 (port is an arbitrary choice).
+  // set self to the scope of the class
+  var self = this;  
+  
+  /*  ================================================================  */
+  /*  App server functions (main app logic here).                       */
+  /*  ================================================================  */
+
+  self.app = express();
+  //	self.app.use(connect(connect.basicAuth('j', 'jmjm')))
+  self.app.use(morgan('[:date] :method :url :status'));	// Log requests
+  self.app.use(express.static(path.join(__dirname, 'public')));	// Process static files
+
+  // Start the server (starts up the sample application).
+  self.start = function() {
+    /*
+     * OpenShift will provide environment variables indicating the IP 
+     * address and PORT to use.  If those variables are not available
+     * (e.g. when you are testing the application on your laptop) then
+     * use default values of localhost (127.0.0.1) and 33333 (arbitrary).
+     */
+    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+    self.port      = process.env.OPENSHIFT_NODEJS_PORT || 33333;
+
+    //  Start listening on the specific IP and PORT
+    self.app.listen(self.port, self.ipaddress, function() {
+      console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+    });
+  };
+}; 
+
+
+/**
+ *  main():  Main code.
  */
-ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-port = process.env.OPENSHIFT_NODEJS_PORT || 33333;
+var sss = new SimpleStaticServer();
+sss.start();
 
-//  Start listening on the specific IP and PORT
-app.listen(port, ipaddress, function() {
-  console.log('%s: Simple static content server started on %s:%d ...', Date(Date.now()), ipaddress, port);
-});
